c258da43bc1ece5f0d1aacd76870992763b9f5ba
Mavdavis <davidmarvelous@seamlesshr.com>
2024-02-19 20:35:50 +0100

    learning resumed

---
+++ project1.py
@@ -0,0 +1,11 @@
+# importing random function
+import random
+
+# define a function
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.radiant(min_value, max_value)
+    return roll
+
+    value = roll()
