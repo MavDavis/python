8374d31a4ca594375071a321eef56b4d27106eed
Mavdavis <davidmarvelous@seamlesshr.com>
2024-02-29 08:54:44 +0100

    wip

--- project1.py
+++ project1.py
@@ -5,7 +5,8 @@ import random
 def roll():
     min_value = 1
     max_value = 6
-    roll = random.radiant(min_value, max_value)
+ #   roll = random.radiant(min_value, max_value)
     return roll
     
 value = roll()
+print(random)
