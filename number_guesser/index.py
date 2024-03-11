ad183014dfa18252116132f8118c451c8574fc34
Mavdavis <davidmarvelous@seamlesshr.com>
2024-03-11 16:33:02 +0100

    number guesses

--- number_guesser/index.py
+++ number_guesser/index.py
@@ -1,7 +1,7 @@
 import random
 
-top_of_number = input("Enter a number: ")
-# 
+top_of_number = input("Enter max number: ")
+
 if top_of_number.isdigit():
     top_of_number = int(top_of_number)
     if top_of_number <=0:
@@ -10,5 +10,23 @@ if top_of_number.isdigit():
 else:
     print("Please enter a number next time")
     quit()
-random_number = random.randint(-5,11)
-print(random_number) 
+number_of_guesses = 0
+random_number = random.randint(0,top_of_number)
+while True:
+   number_of_guesses +=1
+   guess = input("Enter a number: ")
+   if guess.isdigit():
+       guess=int(guess)
+   else:
+       print("Please enter a number")        
+       continue
+   if guess == random_number:
+       print("you got it")
+       break
+   elif guess < random_number:
+       print("greater than your guess, try again")
+   else: 
+       print("less than your guess, try again")
+
+print("you had ", number_of_guesses,"number of guesses")
+
