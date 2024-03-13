6690cb1749a3f5ee6a4e1007235a6e579b4295f1
Mavdavis <davidmarvelous@seamlesshr.com>
2024-03-13 08:49:25 +0100

    wip: rock paper, scissors

--- number_guesser/rock_papar_scissors/index.py
+++ number_guesser/rock_papar_scissors/index.py
@@ -1,9 +1,13 @@
 import random
 user_wins = 0
 computer_wins = 0
+options = ["Rock", "Paper", "Scissors"]
 while True:
     user_input = input("Enter Rock, Paper, Scissors or q to Quit: ").lower()
     if user_input == 'q':
         break
-    if user_input not in ["Rock", "Paper", "Scissors"]:
+    if user_input not in options:
         continue
+    random_number = random.randint(0,2)
+    # 0=rock, 1=paper, 2=scissors;
+print("Goodbye")
